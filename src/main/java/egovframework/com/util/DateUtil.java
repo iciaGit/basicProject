4a2a0a28a2d17b8289b00e19db0c210296f24a0d
김지훈 <jhkim@innerinfo.net>
2019-08-08 16:01:53 +0900

    데이트 유틸 추가

---
+++ src/main/java/egovframework/com/util/DateUtil.java
@@ -0,0 +1,27 @@
+package egovframework.com.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class DateUtil {
+
+	private static Calendar cal = Calendar.getInstance(Locale.KOREA);
+	private static SimpleDateFormat sdf =null;
+	
+	//현재 연 구하기
+	public static int getcurrentYear() {
+		return cal.get(Calendar.YEAR);
+	}
+	
+	//현재 월 구하기
+	public static int getcurrentMonth() {
+		return cal.get(Calendar.MONTH)+1;
+	}
+	
+	//현재 일 구하기
+	public static int getcurrentDay() {		
+		return cal.get(Calendar.DAY_OF_MONTH);
+	}
+	
+}
