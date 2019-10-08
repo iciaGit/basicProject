0baf175bf85dc104f2fc741e0932ea5eac9102f7
김지훈 <jhkim@innerinfo.net>
2019-10-08 16:18:02 +0900

    유틸리티 추가(Xss 필터, http 통신 유틸 추가)

---
+++ src/main/java/egovframework/com/util/XssUtil.java
@@ -0,0 +1,46 @@
+package egovframework.com.util;
+
+public class XssUtil {
+	
+	/** XSS 스트립트 방지(기본)
+	 * 
+	 * @param value:String
+	 * @return returnValue:String
+	 */
+	public static String clearXssMin(String value) {
+		
+		if (value == null || value.trim().equals("")) {
+			return "";
+		}
+
+		String returnValue = value;
+
+		returnValue = returnValue.replaceAll("&", "&amp;");
+		returnValue = returnValue.replaceAll("<", "&lt;");
+		returnValue = returnValue.replaceAll(">", "&gt;");
+		returnValue = returnValue.replaceAll("\"", "&#34;");
+		returnValue = returnValue.replaceAll("\'", "&#39;");
+		returnValue = returnValue.replaceAll(".", "&#46;");
+		returnValue = returnValue.replaceAll("%2E", "&#46;");
+		returnValue = returnValue.replaceAll("%2F", "&#47;");
+		return returnValue;
+	}
+	
+	/** XSS 스트립트 방지(최대치)
+	 * 
+	 * @param value:String
+	 * @return returnValue:String
+	 */
+	public static String clearXssMax(String value) {
+		String returnValue = value;
+		returnValue = clearXssMin(returnValue);//기본을 통과 후...
+		returnValue = returnValue.replaceAll("%00", null);
+		returnValue = returnValue.replaceAll("%", "&#37;");
+		returnValue = returnValue.replaceAll("\\.\\./", ""); // ../
+		returnValue = returnValue.replaceAll("\\.\\.\\\\", ""); // ..\
+		returnValue = returnValue.replaceAll("\\./", ""); // ./
+		returnValue = returnValue.replaceAll("%2F", "");
+		return returnValue;
+	}	
+
+}
